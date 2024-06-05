9bb6f75cece810f86c465f7f13e07f543ec8dfc6
Emanuele <ema@localhost.localdomain>
2024-06-05 13:45:26 +0200

    On branch iota  Changes to be committed: 	new file:   ../.gitmodules 	new file:   components/iota_c 	modified:   main/com_node_iota.c 	modified:   main/com_node_iota.h 	new file:   secure_com_node/components/iota_c

--- secure_com_node/main/com_node_iota.c
+++ secure_com_node/main/com_node_iota.c
@@ -1,13 +1,91 @@
 #include "com_node_iota.h"
 
-int addHashToBlockchain(unsigned char * msg, size_t msg_size) {
+int addHashToBlockchain(unsigned char * msg, size_t msg_size, char * block_id) {
   // Calculate SHA256 Hash
   unsigned char * hash_buffer = (unsigned char *) calloc(UTILS_SHA256_OUTPUT_SIZE, sizeof(unsigned char));
 
   calculateSHA256Hash(msg, msg_size, hash_buffer, UTILS_SHA256_OUTPUT_SIZE);
 
   // Send data to Iota blockchain
+  if (sendDataBlock(msg, msg_size, block_id) != IOTA_SUCCESS) {
+    ESP_LOGI(LOG_TAG, "Failed to send data to IOTA testnet");
+    return IOTA_FAILURE;
+  }
 
   // cleanup
   free(hash_buffer);
+
+  return IOTA_SUCCESS;
+}
+
+int sendDataBlock(unsigned char * data, size_t data_size, char * block_id) {
+  // config context
+  iota_client_conf_t ctx = {
+    .host = IOTA_HOST,
+    .port = IOTA_PORT,
+    .use_TLS = IOTA_USE_TLS,
+  };
+
+  res_send_block_t res = {0};
+  ESP_LOGI(LOG_TAG, "Sending hash to tangle...");
+  if (send_tagged_data_block(&ctx, 2, (byte_t *)IOTA_TAG, strlen(IOTA_TAG), (byte_t *) data, data_size, &res) == 0) {
+    if (res.is_error) {
+      ESP_LOGI(LOG_TAG, "IOTA API Failure : %s", res.u.error->msg);
+      return IOTA_FAILURE;
+    }
+  } else {
+    ESP_LOGI(LOG_TAG, "IOTA API Generic Failure!");
+    return IOTA_FAILURE;
+  }
+  
+  ESP_LOGI(LOG_TAG, "Hash block succesfully sent.");
+  ESP_LOGI(LOG_TAG, "Block ID: %s", res.u.blk_id);
+
+  *block_id = res.u.blk_id;
+
+  return IOTA_SUCCESS;
+}
+
+int retrieveDataBlock(char * block_id, char * block_data) {
+  res_block_t *blk = res_block_new();
+
+  if (!blk) {
+    ESP_LOGI(LOG_TAG, "Failed to create response block object");
+    return IOTA_FAILURE;
+  }
+
+  ESP_LOGI(LOG_TAG, "Fetching hash data from tangle...");
+  if (get_block_by_id(&ctx, block_id, blk) == 0) {
+    if (blk->is_error) {
+      ESP_LOGI(LOG_TAG, "IOTA API Failure: %s", blk->u.error->msg);
+      res_block_free(blk);
+      return IOTA_FAILURE;
+    }
+  } else {
+    ESP_LOGI(LOG_TAG, "IOTA API Failure : %s", res.u.error->msg);
+    res_block_free(blk);
+    return IOTA_FAILURE;
+  }
+
+  ESP_LOGI(LOG_TAG, "hash block succesfully fetched");
+  
+  // Check if it is an actual tagged data block
+  if (blk->u.blk->payload_type != CORE_BLOCK_PAYLOAD_TAGGED) {
+    ESP_LOGI(LOG_TAG, "Fetched block not a tagged data block!");
+    res_block_free(blk);
+    return IOTA_FAILURE;
+  }
+
+  // Convert block payload to string
+  if (!byte_buf2str(((tagged_data_payload_t *)blk->u.blk->payload)->data)) {
+    ESP_LOGI("Failed to convert payload into string");
+    res_block_free(blk);
+    return IOTA_FAILURE;
+  }
+  
+  *block_data = ((tagged_data_payload_t *)blk->u.blk->payload)->data->data;
+
+  res_block_free(blk);
+
+  return IOTA_SUCCESS;
 }
--- secure_com_node/main/com_node_iota.h
+++ secure_com_node/main/com_node_iota.h
@@ -1,10 +1,37 @@
 #pragma once
 
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
+
+#include "client/api/restful/get_block.h"
+#include "client/api/restful/send_tagged_data.h"
+#include "core/models/payloads/tagged_data.h"
+#include "core/utils/iota_str.h"
+
 #include "com_node_utils.h"
+
+// IOTA CONTEXT MACROS
+#define IOTA_HOST localhost
+#define IOTA_PORT 14265
+#define IOTA_USE_TLS false
+#define IOTA_TAG  "iota.c\xF0\x9F\xA6\x8B"
+
+#define LOG_TAG "IOTA"
 
 enum transactionResult {
   IOTA_SUCCESS,
   IOTA_FAILURE
 };
 
-int addHashToBlockchain(unsigned char * hash_buf, size_t hash_size);
+// Returns block id, meant to be "user facing"
+int addHashToBlockchain(unsigned char * msg, size_t msg_size, char * block_id);
+
+// Returns block id, overwrites block id, which is assumedly already allocated
+int sendDataBlock(unsigned char * data, size_t data_size, char * block_id);
+
+// Returns IOTA SUCCESS io IOTA FAILURE
+int retrieveDataBlock(char * block_id, char * block_data);
